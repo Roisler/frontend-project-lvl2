843f4db2e675004571d2c1cbecaa1935cf32ea87
roisler <roislerxs@gmail.com>
2022-05-09 20:34:55 +0700

    edit gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,9 +1,15 @@
-import { Command } from 'commander';
+import { program } from 'commander';
 
-const genDiff = new Command();
-genDiff
+const genDiff = (filepath1, filepath2) => {
+  console.log(filepath1);
+  console.log(filepath2);
+};
+
+program
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number');
-genDiff.parse();
+  .option('-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>', 'Enter filepath')
+  .option('-f, --format <type>', 'output format');
+program.parse();
 
 export default genDiff;
