2f6f286879ff6e6bcb4445e6b607036a9d8970d3
roisler <roislerxs@gmail.com>
2022-06-01 17:27:09 +0700

    fix problems with path

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import path, { dirname } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 import yml from 'js-yaml';
 
@@ -7,20 +7,20 @@ import genDiff from '../src/gendiff.js';
 import { parseFile } from '../src/parsers.js';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf-8');
+const readFile = (file) => fs.readFileSync(file, 'utf-8');
 
-const resultStylish = readFile('stylish.txt');
-const resultPlain = readFile('plain.txt');
-const resultJSON = readFile('json.json');
+const resultStylish = readFile(getFixturePath('stylish.txt'));
+const resultPlain = readFile(getFixturePath('plain.txt'));
+const resultJSON = readFile(getFixturePath('json.json'));
 
-const file1 = 'file1.json';
-const file2 = 'file2.json';
-const file3 = 'file1.yml';
-const file4 = 'file2.yml';
+const file1 = getFixturePath('file1.json');
+const file2 = getFixturePath('file2.json');
+const file3 = getFixturePath('file1.yml');
+const file4 = getFixturePath('file2.yml');
 
 test('gendiff', () => {
   expect(genDiff(file1, file2)).toEqual(resultStylish);
--- src/gendiff.js
+++ src/gendiff.js
@@ -4,14 +4,14 @@ import { resolve } from 'path';
 import { format, parseFile } from './parsers.js';
 import getDiffOfFormat from './formatters/index.js';
 
-const getFixturePath = (filename) => resolve(cwd(), './__fixtures__/', filename);
+const getFullPath = (filepath) => resolve(cwd(), filepath);
 
 const genDiff = (filepath1, filepath2, formatter = 'stylish') => {
   if (filepath1 === '' || filepath2 === '') {
     return 'Specify the file to be compared';
   }
-  const file1 = parseFile(readFileSync(getFixturePath(filepath1), 'utf-8'), format(filepath1));
-  const file2 = parseFile(readFileSync(getFixturePath(filepath2), 'utf-8'), format(filepath2));
+  const file1 = parseFile(readFileSync(getFullPath(filepath1), 'utf-8'), format(filepath1));
+  const file2 = parseFile(readFileSync(getFullPath(filepath2), 'utf-8'), format(filepath2));
   return getDiffOfFormat(file1, file2, formatter);
 };
 
