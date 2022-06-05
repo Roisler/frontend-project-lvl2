ebad0ec166aa09d6cfff8ea8f9d7a40a4cc1704f
roisler <roislerxs@gmail.com>
2022-06-05 16:55:50 +0700

    fix code climate

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -13,28 +13,32 @@ const stringify = (value) => {
 const plain = (tree) => {
   const iter = (node, path) => {
     const result = node.flatMap((element) => {
-      if (element.controlChanged === 'added') {
-        const fullPath = path + element.key;
-        const val = stringify(element.value);
-        return `Property '${fullPath}' was added with value: ${val}`;
+      switch (element.controlChanged) {
+        case 'added': {
+          const fullPath = path + element.key;
+          const val = stringify(element.value);
+          return `Property '${fullPath}' was added with value: ${val}`;
+        }
+        case 'removed': {
+          const fullPath = path + element.key;
+          return `Property '${fullPath}' was removed`;
+        }
+        case 'unchanged': {
+          return '';
+        }
+        case 'changed': {
+          const value1 = stringify(element.value1);
+          const value2 = stringify(element.value2);
+          const fullPath = path + element.key;
+          return `Property '${fullPath}' was updated. From ${value1} to ${value2}`;
+        }
+        case 'nested': {
+          return iter(element.children, `${path}${element.key}.`);
+        }
+        default: {
+          return '';
+        }
       }
-      if (element.controlChanged === 'removed') {
-        const fullPath = path + element.key;
-        return `Property '${fullPath}' was removed`;
-      }
-      if (element.controlChanged === 'unchanged') {
-        return '';
-      }
-      if (element.controlChanged === 'changed') {
-        const value1 = stringify(element.value1);
-        const value2 = stringify(element.value2);
-        const fullPath = path + element.key;
-        return `Property '${fullPath}' was updated. From ${value1} to ${value2}`;
-      }
-      if (element.controlChanged === 'nested') {
-        return iter(element.children, `${path}${element.key}.`);
-      }
-      return '';
     })
       .filter((element) => (element !== ''));
     return result.join('\n');
